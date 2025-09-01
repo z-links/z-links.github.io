33ede41d65f8610e7082f38423c0e99e898e06bb
msabaq <mhdsabaq@outlook.com>
2025-09-01 19:28:50 +0530

    fix: improvised and corrected the footer

--- src/components/TreeBase.svelte
+++ src/components/TreeBase.svelte
@@ -186,7 +186,11 @@
             btn.disabled = true;
             btn.innerHTML = '<svg class="animate-spin h-4 w-4 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="m4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg> Submitting...';
             
-            url = await editMethods.submitChanges(isNewTree, title, url);
+            try {
+              url = await editMethods.submitChanges(isNewTree, title, url);
+            } catch (err) {
+              console.error("Error submitting changes", err);
+            }
             
             btn.disabled = false;
             btn.innerHTML = 'Submit';
@@ -201,3 +205,5 @@
   {/key}
 </div>
 <ShareModal url={url} isOpen={shareModalOpen} />
+
+
